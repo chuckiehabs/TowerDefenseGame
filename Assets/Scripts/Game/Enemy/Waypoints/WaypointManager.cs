42223dcbea9e578f8b59b1b48542b898856878b4
chuckiehabs <cpearson_99_ca@hotmail.com>
2018-11-15 11:20:10 -0500

    Waypoints incomplete

---
+++ Assets/Scripts/Game/Enemy/Waypoints/WaypointManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class WaypointManager : MonoBehaviour {
+
+    public static WaypointManager Instance;
+    public List<Path> Paths = new List<Path>();
+    void Awake()
+    {
+        Instance = this;            
+    }
+
+    public Vector3 GetSpawnPosition(int pathIndex)
+    {
+        return Paths[pathIndex].WayPoints[0].position;
+    }
+
+
+    [System.Serializable]                                           // makes this available in the editor
+    public class Path
+    {
+        public List<Transform> WayPoints = new List<Transform>();   // creates a dictionary
+    }
+    
+}
