1f3a978402432b46436b3ec8d1a361c3c232b253
chuckiehabs <cpearson_99_ca@hotmail.com>
2018-11-29 10:14:47 -0500

    Made StoneTower script and Tower script

---
+++ Assets/Scripts/Game/Enemy/Wave/WaveManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+    //1
+    public static WaveManager Instance;
+    //2
+    public List<EnemyWave> enemyWaves = new List<EnemyWave>();
+    //3
+    private float elapsedTime = 0f;
+    //4
+    private EnemyWave activeWave;
+    //5
+    private float spawnCounter = 0f;
+    //6
+    private List<EnemyWave> activateWaves = new List<EnemyWave>();
+
+    //1
+    void Awake()
+    {
+        Instance = this;
+    }
+    //2
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        SearchForWave();
+        UpdateActiveWave();
+    }
+    private void SearchForWave()
+    {
+        //3
+        foreach (EnemyWave enemyWave in enemyWaves)
+        {
+            //4
+            if (!activateWaves.Contains(enemyWave)
+            && enemyWave.startSpawnTimeInSeconds <= elapsedTime)
+            {
+
+                //5
+                activeWave = enemyWave;
+                activateWaves.Add(enemyWave);
+                spawnCounter = 0f;
+                GameManager.Instance.waveNumber++;
+                //6
+                break;
+            }
+        }
+    }
+    //7
+    private void UpdateActiveWave()
+    {
+        //1
+        if (activeWave != null)
+        {
+            spawnCounter += Time.deltaTime;
+            //2
+            if (spawnCounter >= activeWave.timeBetweenSpawnsInSeconds)
+            {
+                spawnCounter = 0f;
+                //3
+                if (activeWave.listOfEnemies.Count != 0)
+                {
+                    //4
+                    GameObject enemy = (GameObject)Instantiate(
+                    activeWave.listOfEnemies[0], WayPointManager.Instance.
+                    GetSpawnPosition(activeWave.pathIndex), Quaternion.identity);
+                    //5
+                    enemy.GetComponent<Enemy>().pathIndex = activeWave.pathIndex;
+                    //6
+                    activeWave.listOfEnemies.RemoveAt(0);
+                }
+                else
+                {
+                    //7
+                    activeWave = null;
+                    //8
+                    if (activateWaves.Count == enemyWaves.Count)
+                    {
+                        // All waves are over
+                        GameManager.Instance.enemySpawningOver = true;
+                    }
+                }
+            }
+
+            //public void stopSpawning()
+            //{
+            //    elapsedTime = 0;
+            //    spawnCounter = 0;
+            //    activeWave = null;
+            //    activateWaves.Clear();
+            //    enabled = false;
+            //}
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Game/Tower/StoneTower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneTower :  Tower
+{
+    //1
+    public GameObject stonePrefab;
+    //2
+    protected override void AttackEnemy()
+    {
+        base.AttackEnemy();
+        //3
+        GameObject stone = (GameObject)Instantiate(stonePrefab, towerPieceToAim.position, Quaternion.identity);
+    }
+
+}
